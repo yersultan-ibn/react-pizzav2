9bbf2c0cf09a485f4e339da9188bfad8debc97a6
Ersultan <example@gmail.com>
2022-11-15 14:35:35 +0300

    first commit

---
+++ src/components/Categories.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react';
+function Categories() {
+  const [] = useState();
+  return (
+    <div className="categories">
+      <ul>
+        <li className="active">Все</li>
+        <li>Мясные</li>
+        <li>Вегетарианская</li>
+        <li>Гриль</li>
+        <li>Острые</li>
+        <li>Закрытые</li>
+      </ul>
+    </div>
+  );
+}
+
+export default Categories;
